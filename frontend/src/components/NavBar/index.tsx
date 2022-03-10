84534627fcfaee80eebeeaa3d96be3e57849e442
Ricardo Lopes <ralopesz@gmail.com>
2022-03-09 23:22:34 -0300

    Navbar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,25 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function NavBar() {
+
+    return (
+
+        <header>
+            <nav className="container">
+                <div className="dsname-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/devsuperior">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link"> devsuperior </p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+
+    )
+}
+
+export default NavBar;
